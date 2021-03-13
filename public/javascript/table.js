880ba7d4156551a693975bbacb5ad752ab5f6587
Ian Eustis <moose@Caramel-Corn.local>
2021-03-12 16:46:21 -0800

    css edits

---
+++ public/javascript/table.js
@@ -0,0 +1,8 @@
+function copy() {
+  var copyText = document.querySelector("#url");
+  copyText.select();
+  document.execCommand("copy");
+  console.log("copied")
+}
+
+document.querySelector("#copy_url").addEventListener("click", copy);
